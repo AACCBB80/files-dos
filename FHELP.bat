e2f12bd36917ab85671a4ae97c24c0e9710a9b4a
AACCBB80 <89084279+AACCBB80@users.noreply.github.com>
2022-02-02 12:31:04 -0600

    Add files via upload

---
+++ FHELP.bat
@@ -0,0 +1,10 @@
+echo off
+cls
+echo =Help=Menu=====================================================================
+echo If you bought this program, you should DEMAND YOUR MONEY BACK
+echo (Reason: This is free.)
+echo Type FILES * for all files, or leave it FILES for folders.
+Echo You can also enter a specific file format like this: FILES COM
+echo Please share this program with anyone who has DOS, it would make me happy! :) 
+echo ===============================================================================
+echo on
---
+++ FILES.bat
@@ -0,0 +1,10 @@
+echo off
+set directory="cd"
+cls
+echo =Shareware=Version=============================================================
+echo You are in the
+dir *.%1
+echo ===============================================================================
+echo Type a program with it's file extention (Example: COMMAND.COM)
+echo FHELP for help.
+echo on
